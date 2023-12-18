45ab176f1adfc3095cbc592bd6ca4a1f417c69ae
Sudha <sudhamuthusamy17@gmail.com>
2023-12-18 15:38:32 +0530

    Pull the prgm

---
+++ src/src/dec18/sudha.java
@@ -0,0 +1,14 @@
+package src.dec18;
+
+public class sudha {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 25;
+        if (a > b) {
+            System.out.println("A is bigger than B");
+        }else {
+            System.out.println("A is smaller than B");
+        }
+    }
+    }
+
