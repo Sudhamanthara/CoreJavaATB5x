31a4d6897c85cdbdd8a3545d9ac6259119df13f1
Sudha <sudhamuthusamy17@gmail.com>
2023-12-18 11:22:07 +0530

    my first commit on dec 17

---
+++ src/Lab001.java
@@ -0,0 +1,8 @@
+public class Lab001 {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        String name = "Sudha Mantharasalam";
+        System.out.printf("My Name is " + name);
+    }
+}
---
+++ src/src/dec17/LAB001.java
@@ -0,0 +1,10 @@
+package src.dec17;
+
+public class LAB001 {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        String name = "Sudha Mantharasalam";
+        System.out.printf("My Name is " + name);
+    }
+}
