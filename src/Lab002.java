3589bcd94ef0cec4f6eadab0b71d6bdcb0a1c0cc
Sudha <sudhamuthusamy17@gmail.com>
2023-12-18 12:35:19 +0530

    Pull the prgm

---
+++ src/Lab002.java
@@ -0,0 +1,9 @@
+public class Lab002 {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        String name1 = "Sudha";
+        String name2 = "Mantharasalam";
+        System.out.printf("My FullName is " + name1 + name2);
+    }
+}
