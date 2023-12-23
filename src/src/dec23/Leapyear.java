c481a88d497ad4c5b33e6592887ec468e516ab43
Sudha <sudhamuthusamy17@gmail.com>
2023-12-23 18:47:47 +0530

    Pull the prgm

--- src/src/dec23/Leapyear.java
+++ src/src/dec23/Leapyear.java
@@ -4,15 +4,13 @@ import java.util.Scanner;
 
 public class Leapyear {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the year");
-        int year = sc.nextInt();
-        if ((year % 400 == 0)|| ((year % 4 == 0) && (year % 100 == 0)))
+        int year = 2000;
+        if ((year % 400 == 0)|| ((year % 4 == 0) && (year % 100 != 0)))
         {
-            System.out.println("It is a Leap year");
+            System.out.println("True !! It is a Leap year");
         }
         else{
-            System.out.println("It is not a Leap year");
+            System.out.println("False .. It is not a Leap year");
         }
     }
 }
